40c100426d57024046945f7d96aa6a55683eb0f0
Larissa Ruster Vesper Cruz <lrcruz@grupogbi.com>
2022-12-05 14:48:26 -0300

    Feat: Initial Commit

---
+++ implement-with-redis/implement-with-redis/Controllers/CountriesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Net.Http;
+using Newtonsoft.Json;
+using implement_with_redis.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace implement_with_redis.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+        private const string CountriesKey = "Countries";
+
+        public CountriesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            var countriesObject = await _distributedCache.GetStringAsync(CountriesKey);
+
+            if (!string.IsNullOrWhiteSpace(countriesObject))
+            {
+                return Ok(countriesObject);
+            }
+            else
+            {
+                const string restCountriesUrl = "https://restcountries.com/v3.1/all";
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync(restCountriesUrl);
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    var countries = JsonConvert.DeserializeObject<List<Country>>(responseData);
+                    var memoryCacheEntryOptions = new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                        SlidingExpiration = TimeSpan.FromSeconds(1200)
+                    };
+
+                    await _distributedCache.SetStringAsync(CountriesKey, responseData, memoryCacheEntryOptions);
+
+                    return Ok(countries);
+                }
+            }
+
+        }
+    }
+}
---
+++ implement-with-redis/implement-with-redis/Models/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace implement_with_redis.Models
+{
+    public class Country
+    {
+        public Name name { get; set; }
+        public string[] capital { get; set; }
+        public string region { get; set; }
+        public string subregion { get; set; }
+    }
+
+    public class Name
+    {
+        public string common { get; set; }
+        public string official { get; set; }
+    }
+}
